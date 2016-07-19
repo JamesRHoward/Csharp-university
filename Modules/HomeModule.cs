3e1b564645628a573839f324265589146ac96c7e
Bhawani Parajuli and James Howard <bhawani+james@>
2016-07-19 16:02:02 -0700

    working many to many relationship

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -34,10 +34,12 @@ namespace University
       };
       Get["courses/{id}"]= parameter =>{
         Dictionary<string, object> course_student = new Dictionary<string, object>();
-        var getCourse = Course.Find(parameter.id);
-        var getStudent = getCourse.GetStudents();
+        Course getCourse = Course.Find(parameter.id);
+        List<Student> courseStudent = getCourse.GetStudents();
+        List<Student> AllStudents = Student.GetAll();
         course_student.Add("course", getCourse);
-        course_student.Add("student", getStudent);
+        course_student.Add("courseStudent", courseStudent);
+        course_student.Add("allStudents", AllStudents);
         return View["student_list.cshtml", course_student];
       };
       Post["/students/add"] = _ =>{
@@ -47,6 +49,24 @@ namespace University
         foundCourse.AddStudent(addedStudent);
         List<Student> allStudents = Student.GetAll();
         return View["success.cshtml", allStudents];
+      };
+      Get["/students/{id}"]= parameter =>{
+        Dictionary<string, object> student_course = new Dictionary<string, object>();
+        Student getStudent = Student.Find(parameter.id);
+        List<Course> studentCourse = getStudent.GetCourses();
+        List<Course> AllCourses = Course.GetAll();
+        student_course.Add("student", getStudent);
+        student_course.Add("studentCourse", studentCourse);
+        student_course.Add("allCourses", AllCourses);
+        return View ["course_list.cshtml", student_course];
+      };
+      Post["/courses/add"] = _ =>{
+        Student foundStudent = Student.Find(Request.Form["student_id"]);
+        Course addedCourse = new Course(Request.Form["course_name_add"], Request.Form["course_number_add"]);
+        addedCourse.Save();
+        foundStudent.AddCourse(addedCourse);
+        List<Course> allCourses = Course.GetAll();
+        return View["success.cshtml", allCourses];
       };
     }
   }
